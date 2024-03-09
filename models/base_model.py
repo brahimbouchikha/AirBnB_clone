29016df5491eb4a16197e2b6c8f642925715f8c2
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-03-09 23:40:57 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """create a class"""
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
     """represent a class"""
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         """Initialize a class"""
-        self.uuid = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.uuid = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
-        return f"[BaseModel] ({self.uuid}) {self.__dict__}"
+        """Return the str representation of BaseModel instance."""
+        className = self.__class__.__name__
+        return f"[{className}] ({self.id}) {self.__dict__}"
