6d824729af0e61566ec1e3e4000fe477fbfc81d8
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-05-18 14:05:03 +0100

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,6 @@ file_storage Module
 
 import json
 from models.base_model import BaseModel
-from models import storage 
 import os
 
 
@@ -54,16 +53,4 @@ class FileStorage:
                 except Exception:
                     pass
 
-if __name__ == "__main__":
-    all_objs = storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
 
-    print("-- Create a new object --")
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    my_model.save()
-    print(my_model)
