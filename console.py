7ef7b03d73cce7da7c5a60cd7efefbc22c351fc9
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-05-19 12:48:42 +0100

    update

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Console Module """
 
-
 import cmd
 import sys
 from models.base_model import BaseModel
@@ -15,37 +14,37 @@ from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-	"""
-	Contains the functionality for the HBNB console.
-	"""
+    """
+    Contains the functionality for the HBNB console.
+    """
 
-	prompt = '(hbnb) '
+    prompt = '(hbnb) '
 
-	def do_quit(self, command):
-		"""
-		Method to exit the HBNB console.
-		"""
-		return True
+    def do_quit(self, command):
+        """
+        Method to exit the HBNB console.
+        """
+        return True
 
-	def help_quit(self):
-		"""
-		Prints the help documentation for quit.
-		"""
-		print("Quit command to exit the program\n")
+    def help_quit(self):
+        """
+        Prints the help documentation for quit.
+        """
+        print("Quit command to exit the program\n")
 
-	def do_EOF(self, arg):
-		"""
-		Handles EOF to exit program.
-		"""
-		print()
-		return True
+    def do_EOF(self, arg):
+        """
+        Handles EOF to exit program.
+        """
+        print()
+        return True
 
-	def help_EOF(self):
-		"""
-		Prints the help documentation for EOF.
-		"""
-		print("Exits the program without formatting\n")
+    def help_EOF(self):
+        """
+        Prints the help documentation for EOF.
+        """
+        print("Exits the program without formatting\n")
 
 
 if __name__ == "__main__":
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
