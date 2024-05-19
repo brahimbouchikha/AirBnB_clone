2bd10082aecc1680e205f2291adb24d6923bd106
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-05-19 12:23:41 +0100

    update

--- console.py
+++ console.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Console Module """
+
+
 import cmd
 import sys
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -9,22 +9,22 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 class TestFileStorageInstantiation(unittest.TestCase):
-	"""
+    """
         Testing instantiation of file storage.
-    	"""
+        """
 
-	def test_FileStorage_instantiation_no_args(self):
-		storage = FileStorage()
-		self.assertEqual(type(storage), FileStorage)
+    def test_FileStorage_instantiation_no_args(self):
+        storage = FileStorage()
+        self.assertEqual(type(storage), FileStorage)
 
-	def test_FileStorage_instantiation_with_arg(self):
-		with self.assertRaises(TypeError):
-			FileStorage(None)
-	def test_FileStorage_file_path_is_str(self):
-		self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+    def test_FileStorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+    def test_FileStorage_file_path_is_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-	def testFileStorage_objects_is_dict(self):
-		self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+    def testFileStorage_objects_is_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
