ee3a0f792a0bd2c0f5d09b832326cd92c3e7d6ff
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-05-22 01:14:21 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,9 @@ class BaseModel:
     """A base class for all hbnb models"""
 
     def __init__(self, *args, **kwargs):
-        """Instantiate a new model."""
+        """
+        Instantiate a new model.
+        """
 
         time_form = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
@@ -38,12 +40,16 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """
+        Update updated_at with the current datetime.
+        """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format."""
+        """
+        Convert instance into dict format.
+        """
         ins_dict = self.__dict__.copy()
         ins_dict["__class__"] = self.__class__.__name__
         ins_dict["created_at"] = self.created_at.isoformat()
@@ -52,15 +58,16 @@ class BaseModel:
 
 
 if __name__ == "__main__":
-    all_objs = storage.all()
-    print("-- Reloaded objects --")
-    for obj_id in all_objs.keys():
-        obj = all_objs[obj_id]
-        print(obj)
+    from models.base_model import BaseModel
 
-    print("-- Create a new object --")
     my_model = BaseModel()
-    my_model.name = "My_First_Model"
+    my_model.name = "My First Model"
     my_model.my_number = 89
+    print(my_model)
     my_model.save()
     print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
